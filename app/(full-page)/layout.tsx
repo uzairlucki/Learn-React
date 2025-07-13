a3260f49f1599d5b34b43bf3b22167a2d8e68a35
Uzair Ahmad <32535552+uzairlucki@users.noreply.github.com>
2025-07-13 11:19:50 +0500

    first commit

---
+++ app/(full-page)/layout.tsx
@@ -0,0 +1,22 @@
+import { Metadata } from 'next';
+import AppConfig from '../../layout/AppConfig';
+import React from 'react';
+
+interface SimpleLayoutProps {
+    children: React.ReactNode;
+}
+
+export const metadata: Metadata = {
+    title: 'Employee Management System',
+    description: 'Employee Management System using PrimeReact and Spring Boot',
+    keywords: 'Employee Management, PrimeReact, Spring Boot, React',
+};
+
+export default function SimpleLayout({ children }: SimpleLayoutProps) {
+    return (
+        <React.Fragment>
+            {children}
+            <AppConfig simple />
+        </React.Fragment>
+    );
+}
---
+++ layout/AppMenu.tsx
@@ -0,0 +1,43 @@
+/* eslint-disable @next/next/no-img-element */
+
+import React, { useContext } from 'react';
+import AppMenuitem from './AppMenuitem';
+import { LayoutContext } from './context/layoutcontext';
+import { MenuProvider } from './context/menucontext';
+import { AppMenuItem } from '@/types';
+
+const AppMenu = () => {
+    const { layoutConfig } = useContext(LayoutContext);
+
+    const model: AppMenuItem[] = [
+
+        {
+            label: 'Pages',
+            icon: 'pi pi-fw pi-briefcase',
+            to: '/pages',
+            items: [
+                
+                
+                {
+                    label: 'Crud',
+                    icon: 'pi pi-fw pi-pencil',
+                    to: '/pages/crud'
+                }
+            ]
+        }
+    ];
+
+    return (
+        <MenuProvider>
+            <ul className="layout-menu">
+                {model.map((item, i) => {
+                    return !item?.seperator ? <AppMenuitem item={item} root={true} index={i} key={item.label} /> : <li className="menu-separator"></li>;
+                })}
+
+                
+            </ul>
+        </MenuProvider>
+    );
+};
+
+export default AppMenu;
